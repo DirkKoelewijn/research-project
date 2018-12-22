d687bfd1bfa8ebc8e0def746f5f191b1ba590352
Dirk Koelewijn <d.koelewijn@student.utwent.nl>
2018-12-22 18:09:16 +0100

    Documented util.py

--- rulegeneration/util.py
+++ rulegeneration/util.py
@@ -1,12 +1,9 @@
-def file_lines(f_name: str) -> [str]:
-    with open(f_name, 'r') as f:
-        return f.readlines()
-
-
-# Return files without end of line
-def file_lines_(f_name: str) -> [str]:
-    return [l.replace('\n', '') for l in file_lines(f_name)]
-
-
 def file_str(f_name: str) -> str:
-    return ''.join(file_lines(f_name))
+    """
+    Reads a file into a string
+
+    :param f_name: Relative path to file
+    :return: File as string
+    """
+    with open(f_name, 'r') as f:
+        return ''.join(f.readlines())
