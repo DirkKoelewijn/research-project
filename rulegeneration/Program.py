8b5f0e90ff421ce1652712d5497e99f1af771627
Dirk Koelewijn <d.koelewijn@student.utwent.nl>
2018-12-22 18:06:18 +0100

    Documented Program

--- rulegeneration/Program.py
+++ rulegeneration/Program.py
@@ -1,26 +1,33 @@
-import Modules
+from Modules import *
 from util import file_str
 
 
 class Program:
+    """
+    Class to generate BPF programs that can access packet fields
+    """
     MOD = "$MODULE_NAME"
+    DEFAULT_MOD = "DDoSMitigation"
     PROG = "$PROG_NAME"
+    DEFAULT_PROG = "xdp_filter"
     CODE = "$CODE"
     POS = "$MATCH"
     NEG = "$NO_MATCH"
 
-    def __init__(self, module: 'Modules.Module', black_list=True):
-        self.__module = module
-        self.__blacklist = black_list
+    @staticmethod
+    def generate(module: Module, code: str = "", blacklist=True, mod_name=DEFAULT_MOD, prog_name=DEFAULT_PROG):
+        """
+        Generates a program with the initial code for the modules and the access code given as a parameter
 
-    def is_blacklist(self):
-        return self.__blacklist
-
-    def set_blacklist(self, blacklist: bool):
-        self.__blacklist = blacklist
-
-    def code(self, mod_name: str, pro_name: str, code: str = "") -> str:
-        template = self.__module.get_final_template()
+        :param module: Module to load in
+        :param code: Code to access the module fields
+        :param blacklist: If true, the program will allow all packets, unless $MATCH is returned. If false, the program
+                          will block all packets except if #MATCH is returned.
+        :param mod_name: Name of the module
+        :param prog_name: Name of the program
+        :return:
+        """
+        template = module.get_final_template()
         code = code.splitlines(False)
 
         # Fix indentation of inserted code
@@ -37,9 +44,9 @@ class Program:
 
         result = "\n".join(filled_template) \
             .replace(Program.MOD, mod_name) \
-            .replace(Program.PROG, pro_name)
+            .replace(Program.PROG, prog_name)
 
-        if self.__blacklist:
+        if blacklist:
             result = result.replace(Program.POS, "XDP_DROP").replace(Program.NEG, "XDP_PASS")
         else:
             result = result.replace(Program.POS, "XDP_PASS").replace(Program.NEG, "XDP_DROP")
@@ -48,4 +55,4 @@ class Program:
 
 
 if __name__ == "__main__":
-    print(Program(Modules.TCPv4).code("module", "xdp_filter", file_str('code/custom_code.c')))
+    print(Program.generate(TCPv4, file_str('code/custom_code.c')))
