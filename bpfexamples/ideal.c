b4f4c19b322233b69aca7f6c5cb6d56b46282153
Dirk Koelewijn <d.koelewijn@student.utwent.nl>
2018-12-23 20:28:40 +0100

    Implemented "ideal" C program to handle multiple protocol rules at once

--- bpfexamples/ideal.c
+++ bpfexamples/ideal.c
@@ -1,7 +1,13 @@
 #define KBUILD_MODNAME "module"
-#include <linux/ip.h>
-#include <linux/tcp.h>
 #include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <linux/ipv6.h>
+#include <linux/tcp.h>
+#include <linux/udp.h>
+#include <linux/icmp.h>
+#include <linux/icmpv6.h>
+#include <linux/igmp.h>
+#include <linux/if_arp.h>
 
 int xdp_filter(struct xdp_md *ctx) {
     // Load pointers to data and end of data
@@ -9,52 +15,95 @@ int xdp_filter(struct xdp_md *ctx) {
     void* data = (void*)(long)ctx->data;
 
     // Possible headers
-    struct ethhdr *eth = NULL;
-    struct iphdr  *ip  = NULL;
-    struct ip6hdr *ip6 = NULL;
-    struct udphdr *udp = NULL;
-    struct tcphdr *tcp = NULL;
+    struct ethhdr   *eth   = NULL;
+    struct iphdr    *ip    = NULL;
+    struct ipv6hdr  *ip6   = NULL;
+    struct udphdr   *udp   = NULL;
+    struct tcphdr   *tcp   = NULL;
+    struct icmphdr  *icmp  = NULL;
+    struct icmp6hdr *icmp6 = NULL;
+    struct igmphdr  *igmp  = NULL;
+    struct arphdr   *arp   = NULL;
 
     // Current offset
     uint64_t offset = 0;
 
-    // OSI 2: Ethernet
+    // OSI 2: Data Link layer
+    // Ethernet
+    uint16_t proto3;
     if (data + offset + sizeof(*eth)  > data_end){
         return XDP_PASS;            // Return if ethernet not available
-        // TODO Go to rules with other layers?
     } else {
         eth = data + offset;
         offset += sizeof(*eth);
+        proto3 = htons(eth->h_proto);
     }
 
-//
-//    // Check if next protocol matches IPv4
-//    if (eth->h_proto == htons(ETH_P_IP)){
-//        ip = data + offset;
-//
-//        // Check if sufficient data
-//        if (data + offset + sizeof(*ip) > data_end)
-//            ip = NULL;
-//
-//        // Increase offset according to header itself
-//        offset += (ip->ihl*4);
-//    }
-//
-//    // Check if next protocol matches TCP
-//        if (ip->protocol == 6){
-//            struct tcphdr *tcp = data + offset;
-//
-//           // Check if sufficient data
-//            if (data + offset + sizeof(*tcp) > data_end)
-//                return XDP_PASS;
-//
-//            // Increase offset according to header itself
-//            offset = offset + (tcp->doff*4);
-//
-//            if (tcp->ack == 1){
-//                bpf_trace_printk("ACK dropped\n");
-//                return XDP_DROP;
-//            }
-//        }
+    // OSI 3: Network layer
+    // IPv4
+    uint8_t proto4;     // Proto: 6 TCP, 17 UDP, 1 ICMP, 2 IGMP
+    if (proto3 == ETH_P_IP){
+        // Return for insufficient data
+        if (data + offset + sizeof(*ip) > data_end)
+            goto Rules;
+
+        ip = data + offset;
+        offset += (ip->ihl*4);
+        proto4 = ip->protocol;
+    }
+    // IPv6
+    else if (proto3 == ETH_P_IPV6){
+        // Return for insufficient data
+        if (data + offset + sizeof(*ip6) > data_end)
+            goto Rules;
+
+        ip6 = data + offset;
+        offset += sizeof(*ip6);
+    }
+
+    // OSI 4: Transport layer
+    // ICMP (1, IPv4)
+    if (proto4 == 1 && proto3 == ETH_P_IP){
+        if (data + offset + sizeof(*icmp) > data_end)
+            goto Rules;
+
+        icmp = data + offset;
+        offset += sizeof(*icmp);
+    }
+    // ICMPv6 (1, IPv6)
+    else if (proto4 == 1 && proto3 == ETH_P_IPV6) {
+        if (data + offset + sizeof(*icmp6) > data_end)
+            goto Rules;
+
+        icmp6 = data + offset;
+        offset += sizeof(*icmp6);
+    }
+    // IGMP (2)
+    else if (proto4 == 2){
+        if (data + offset + sizeof(*igmp) > data_end)
+            goto Rules;
+
+        igmp = data + offset;
+        offset += sizeof(*igmp);
+    }
+    // TCP (6)
+    else if (proto4 == 6){
+        if (data + offset + sizeof(*tcp) > data_end)
+            goto Rules;
+
+        tcp = data + offset;
+        offset += sizeof(*tcp);
+    }
+    // UDP
+    else if (proto4 == 17){
+        if (data + offset + sizeof(*udp) > data_end)
+            goto Rules;
+
+        udp = data + offset;
+        offset += sizeof(*udp);
+    }
+
+    Rules:
+    bpf_trace_printk("TCP == NULL: %d\n", (tcp == NULL));
     return XDP_PASS;
 }
