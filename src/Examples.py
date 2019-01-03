ba3efde1eed929db11f611bf1d0015265c231768
Dirk Koelewijn <d.koelewijn@student.utwent.nl>
2019-01-03 21:45:05 +0100

    Added Examples

---
+++ src/Examples.py
@@ -0,0 +1,14 @@
+from Protocols import IPv4, TCP
+from Rules import Rule
+
+# Easy rule typing
+ips = Rule.one(IPv4['src'] == '1.2.3.4', IPv4['src'] == '2.3.4.5')
+ports = Rule.one(TCP['src'] == 80, TCP['src'] == 82)
+rule = Rule.all(ips, ports)
+print(rule)
+
+# Other notation could be:
+ips = (IPv4['src'] == '1.2.3.4') | (IPv4['src'] == '2.3.4.5')
+ports = (TCP['src'] == 80) | (TCP['src'] == 89)
+rule = Rule(ips & ports)
+print(rule)
