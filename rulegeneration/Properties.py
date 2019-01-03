d7d40dea1229719aaf0c245a77c9b6b2e405a4ab
Dirk Koelewijn <d.koelewijn@student.utwent.nl>
2019-01-03 10:21:09 +0100

    Documented and cleaned up Properties.py

--- rulegeneration/Properties.py
+++ rulegeneration/Properties.py
@@ -1,31 +1,66 @@
 from abc import ABC, abstractmethod
 
 import Functions
-from Protocols import *
+import Protocols
 
 
 class Property(ABC):
+    """
+    Class to model a property of a network packet
+    """
     function = None
 
-    def __init__(self, proto: Protocol, name: str):
+    def __init__(self, proto: Protocols.Protocol, name: str):
         self.proto = proto
         self.name = name
 
     @abstractmethod
     def compare_code(self, comparer, value: str):
+        """
+        Generates the condition to compare the property
+        :param comparer: Comparator to use (e.g: EQ(==), LTE(<=))
+        :param value: The value to compare the value of the property to
+        :return: Single line of code to be used in the condition
+        """
         raise NotImplementedError
 
     def __str__(self):
         return "%s->%s" % (self.proto.struct_name, self.name)
 
 
-class SimpleProperty(Property):
+class SingularProperty(Property):
+    """
+    Class to model a property with a normal comparison that is not attached to a Protocol.
+    The property value will be compared to the value without any conversion.
+    """
 
     def compare_code(self, comparer, value: str):
         return "%s %s %s" % (self.name, comparer, value)
 
 
+class NormalProperty(Property):
+    """
+    Class to model a property with a normal comparison. The property
+    value will be compared to the value without any conversion.
+    """
+
+    def compare_code(self, comparer, value: str):
+        return "%s %s %s" % (self, comparer, value)
+
+
+class HtonsProperty(Property):
+    """
+    Class to model a property where the bytes of the property value should be reversed before comparison.
+    """
+
+    def compare_code(self, comparer, value: str):
+        return "htons(%s) %s %s" % (self, comparer, value)
+
+
 class MacProperty(Property):
+    """
+    Class to model a property holding a MAC address. Specify the value as 'XX:XX:XX:XX:XX:XX' (X = hexadecimal).
+    """
     function = Functions.CompareMAC
 
     def compare_code(self, comparer, value: str):
@@ -34,13 +69,10 @@ class MacProperty(Property):
         return code
 
 
-class HtonsProperty(Property):
-
-    def compare_code(self, comparer, value: str):
-        return "htons(%s) %s %s" % (self, comparer, value)
-
-
 class IpProperty(Property):
+    """
+    Class to model a property that holds an IP address. Specify the value as "C.C.C.C" (C = 0 <= value < 256).
+    """
 
     def compare_code(self, comparer, value: str):
         val = 0
@@ -48,37 +80,3 @@ class IpProperty(Property):
             val = val << 8
             val += int(v)
         return "%s %s %s" % (self, comparer, val)
-
-
-class NumberProperty(Property):
-
-    def compare_code(self, comparer, value: str):
-        return "%s %s %s" % (self, comparer, value)
-
-
-# TODO Add support for other protocols
-LENGTH = SimpleProperty(Ethernet, 'length')
-# TODO Move code below to protocols? (like line below)
-Ethernet.Source = MacProperty(Ethernet, 'h_source')
-ETH_SRC = MacProperty(Ethernet, 'h_source')
-ETH_DST = MacProperty(Ethernet, 'h_dest')
-ETH_PROTO = HtonsProperty(Ethernet, "h_proto")
-IP_SRC = IpProperty(IPv4, 'saddr')
-IP_DST = IpProperty(IPv4, 'daddr')
-IP_LEN = HtonsProperty(IPv4, 'tot_len')
-UDP_SRC = HtonsProperty(UDP, 'source')
-UDP_DST = HtonsProperty(UDP, 'dest')
-UDP_LEN = HtonsProperty(UDP, 'len')
-TCP_SRC = HtonsProperty(TCP, 'source')
-TCP_DST = HtonsProperty(TCP, 'dest')
-TCP_FIN = NumberProperty(TCP, 'fin')
-TCP_SYN = NumberProperty(TCP, 'syn')
-TCP_RST = NumberProperty(TCP, 'rst')
-TCP_PSH = NumberProperty(TCP, 'psh')
-TCP_ACK = NumberProperty(TCP, 'ack')
-TCP_URG = NumberProperty(TCP, 'urg')
-TCP_ECE = NumberProperty(TCP, 'ece')
-TCP_CWR = NumberProperty(TCP, 'cwr')
-
-if __name__ == '__main__':
-    print(TCP_SRC.function)
