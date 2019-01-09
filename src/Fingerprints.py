40e7a09607d374f88a655951828fbf14a2dc110f
Dirk Koelewijn <d.koelewijn@student.utwent.nl>
2019-01-09 13:51:29 +0100

    Documented Fingerprints.py

--- src/Fingerprints.py
+++ src/Fingerprints.py
@@ -5,6 +5,9 @@ from Util import file_str
 
 
 class Fingerprint:
+    """
+    Class for parsing fingerprint files into fingerprint dicts (format: {Property: [values]})
+    """
     __tcp_flags = {
         'F': TCP['fin'],
         'S': TCP['syn'],
@@ -18,6 +21,12 @@ class Fingerprint:
 
     @staticmethod
     def parse(file):
+        """
+        Parses a file into a fingerprint dict
+
+        :param file: Name of the file
+        :return: Fingerprint as {Property: [values]} dict
+        """
         data = json.loads(file_str(file))
         protocol = str(data['protocol']).upper()
 
@@ -28,6 +37,15 @@ class Fingerprint:
 
     @staticmethod
     def parse_default(data, src_ip, src_port, dst_port):
+        """
+        Parses the default parameters of a fingerprint (IPs and ports)
+
+        :param data: Raw fingerprint data from JSON
+        :param src_ip: property that holds the source ip (e.g: IPv4['src'])
+        :param src_port: property that holds the source port (e.g: TCP['src])
+        :param dst_port: property that holds the destination port (e.g: UDP['dst'])
+        :return: Fingerprint containing default properties
+        """
         return {
             src_ip: sorted(data['src_ips']),
             src_port: sorted([int(d) for d in data['src_ports']]),
@@ -36,10 +54,23 @@ class Fingerprint:
 
     @staticmethod
     def parse_udp(data):
+        """
+        Parses an UDP fingerprint
+
+        :param data: JSON data
+        :return: Fingerprint as {Property: [values]} dict
+        """
         return Fingerprint.parse_default(data, IPv4['src'], UDP['src'], UDP['dst'])
 
+    # noinspection PyTypeChecker
     @staticmethod
     def parse_tcp(data):
+        """
+        Parses a TCP fingerprint
+
+        :param data: JSON data
+        :return: Fingerprint as {Property: [values]} dict
+        """
         result = Fingerprint.parse_default(data, IPv4['src'], TCP['src'], TCP['dst'])
 
         # Get TCP flag
