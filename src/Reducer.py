c0c898b9dfb54cec2d0516422d7822b3e74f0c67
Dirk Koelewijn <d.koelewijn@student.utwent.nl>
2019-01-14 09:59:39 +0100

    Added first version of binary aggregation

--- src/Reducer.py
+++ src/Reducer.py
@@ -1,6 +1,7 @@
 import ipaddress
 from copy import deepcopy
 
+from Fingerprints import Fingerprint
 from Protocols import IPv4
 
 
@@ -64,13 +65,75 @@ class Reducer:
         :param dist: Maximal distance between two values to be aggregated (numeric)
         :return: New fingerprint instance with modifications
         """
-        return Reducer.distance_aggregate(fingerprint, IPv4['src'], dist, Reducer.__conv_ip)
+        return Reducer.distance_aggregate(fingerprint, IPv4['src'], dist, Reducer.__ip_to_int)
 
     @staticmethod
-    def __conv_ip(ip):
+    def __ip_to_int(ip):
         """
         Converts an ip address to an integer
         :param ip: IP address as string
         :return: IP address as integer
         """
         return int(ipaddress.IPv4Address(ip))
+
+    @staticmethod
+    def __ip_to_str(ip):
+        return str(ipaddress.IPv4Address(ip))
+
+    @staticmethod
+    def binary_aggregate(fingerprint, prop, bits, conv_func=None, conv_back=None):
+        result = deepcopy(fingerprint)
+        values = result[prop]
+
+        if len(values) <= 1:
+            return result
+
+        # Convert values if applicable
+        if conv_func is not None and conv_back is not None:
+            values = [conv_func(v) for v in values]
+        elif conv_func is not None or conv_back is not None:
+            raise AssertionError("Both conv_func as conv_back should be specified")
+
+        # Aggregate values
+        groups = []
+        group = [values[0]]
+        for v in values[1:]:
+            if (v >> bits) == (values[-1] >> bits):
+                group.append(v)
+            else:
+                if len(group) >= 2:
+                    group = Reducer.__bit_min_max(v, bits)
+                groups.append(group)
+                group = [v]
+
+        if len(group) >= 2:
+            group = Reducer.__bit_min_max(group[0], bits)
+        groups.append(group)
+
+        # Convert values back if necessary
+        if conv_back is not None:
+            groups = [[conv_back(x) for x in g] for g in groups]
+
+        # Reduce lists larger than one to min-max tuples
+        values = [g[0] if len(g) == 1 else (g[0], g[-1]) for g in groups]
+
+        result[prop] = values
+        return result
+
+    @staticmethod
+    def binary_aggregate_ip(fingerprint, bits):
+        return Reducer.binary_aggregate(fingerprint, IPv4['src'], bits, Reducer.__ip_to_int, Reducer.__ip_to_str)
+
+    @staticmethod
+    def __bit_min_max(b, n):
+        b_min = (b >> n) << n
+        b_max = b_min | int('1' * n, 2)
+        print(bin(b_min))
+        print(bin(b_max))
+        return [b_min, b_max]
+
+
+if __name__ == '__main__':
+    f = Fingerprint.parse('fingerprints/02a3a3fc266b09b7645e538efbc1ea11.json')
+    f2 = Reducer.binary_aggregate_ip(f, 14)
+    print()
