0b91c9fc043331b4266a51fd7a66fed22246a47e
Dirk Koelewijn <d.koelewijn@student.utwent.nl>
2019-01-14 14:36:42 +0100

    Refactored names of files and adapted bpf-loader

--- src/code/bpf-loader.py
+++ src/code/bpf-loader.py
@@ -2,26 +2,31 @@ import sys
 
 from bcc import BPF
 
-if len(sys.argv) != 2:
-    print("Usage: sudo python xdp-tracer.py [file name]")
-    exit(1)
+Device = "enp0s25"
+Function = 'xdp_filter'
 
-file_name = sys.argv[1]
-function_name = 'xdp_filter'
 
-device = "enp0s25"
+def load(file, func=Function, dev=Device):
+    # load BPF program
+    print('Loading bpf program')
+    bpf = BPF(src_file=file)
 
-# load BPF program
-print('Loading bpf program')
-bpf = BPF(src_file=file_name)
+    fn = bpf.load_func(func, BPF.XDP)
 
-fn = bpf.load_func(function_name, BPF.XDP)
+    bpf.attach_xdp(dev, fn, 0)
 
-bpf.attach_xdp(device, fn, 0)
+    print("Following traceprint, hit CTRL+C to stop and remove")
+    try:
+        bpf.trace_print()
+    except KeyboardInterrupt:
+        print("\nRemoving filter from device")
+        bpf.remove_xdp(dev, 0)
 
-print("Following traceprint, hit CTRL+C to stop and remove")
-try:
-    bpf.trace_print()
-except KeyboardInterrupt:
-    print("\nRemoving filter from device")
-    bpf.remove_xdp(device, 0)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: sudo python xdp-tracer.py [file name]")
+        exit(1)
+
+    file_name = sys.argv[1]
+    load(file_name)
